2b506d290fbf94fe6123d367b3203fcbd338c273
Ana <117763852+anaglm@users.noreply.github.com>
2023-02-13 12:45:00 -0300

    Add files via upload

---
+++ faculdade_curso.sql
@@ -0,0 +1,45 @@
+USE faculdade;
+
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(17711,"Raissa Ferreiro Ribeiro Pereira", 977639811, 10669374008, 1, 2);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(41886,"Sarah Lima Alves", 936466247, 69600975094, 5, 9);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(33024,"Giovana Luiza Nunes Castro", 925393467, 81752596048, 6, 7);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(76794,"Nicole Carvalho Dias", 970452343, 23464103064, 7, 1);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(06918,"Julian Goncalves Cavalcanti", 971090896, 11404825053, 8, 1);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(85336,"Matheus Gomes Pinto Silveira", 984663809, 52873997079, 3, 5);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(31917,"Leonardo Costa Souza", 952253186, 93493123019, 9, 4);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(52219,"Joao Pedro Martins Pereira", 979387763, 32805582047, 2, 8);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(15338,"Brenda Ferreira Correia", 906887130, 00496886002, 4, 8);
+INSERT INTO  aluno (RA, Nome, Telefone, CPF, Cod_Curso, Cod_Turma ) 
+	VALUE(94566,"Fábio Azevedo Almeida Barros", 938762770, 78331927087, 2, 6);
+    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(21, "Relações Internacionais");   
+INSERT INTO curso  (Cod_Curso, Nome) 
+	VALUE(22, "Letras");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(23, "Engenharia de Produção");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(24, "Design de Moda");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(25, "Engenheiro de dados");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(26, "Fotografia");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(26, "Geografia");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(27, "Mecatrônica Industrial");    
+INSERT INTO curso (Cod_Curso, Nome) 
+	VALUE(28, "Publicidade e Propaganda");    
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(29, "Ciências Contábeis"); 
+INSERT INTO  curso (Cod_Curso, Nome) 
+	VALUE(30, "Ciências Econômicas");    
