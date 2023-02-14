d505dc40be95aadf3b14af9284e005df356ff81a
Ana <117763852+anaglm@users.noreply.github.com>
2023-02-14 16:30:08 -0300

    Add files via upload

---
+++ update.sql
@@ -0,0 +1,29 @@
+USE faculdade;
+  select * from Aluno;
+  select * from Curso;
+  select * from Departamento;
+  select * from Disciplina;
+  select * from Professor;
+  select * from Turma;
+
+update aluno set aluno.Cod_Turma = 1,aluno.Cod_Curso = 1 where aluno.RA = 2372;
+update aluno set aluno.Cod_Turma = 1, aluno.Cod_Curso =  2 where aluno.RA = 2873;
+update aluno set aluno.Cod_Turma = 1, aluno.Cod_Curso =  3 where aluno.RA = 10357;
+update aluno set aluno.Cod_Turma = 4, aluno.Cod_Curso =  4 where aluno.RA = 10425;
+update aluno set aluno.Cod_Turma = 5, aluno.Cod_Curso =  5 where aluno.RA = 10469;
+update aluno set aluno.Cod_Turma = 5, aluno.Cod_Curso =  6 where aluno.RA = 10475;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  7 where aluno.RA = 10469;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  8 where aluno.RA = 10475;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  9 where aluno.RA = 10547;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  10 where aluno.RA = 10573;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  11 where aluno.RA = 10652;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  12 where aluno.RA = 10657;
+update aluno set aluno.Cod_Turma = 2, aluno.Cod_Curso =  13 where aluno.RA = 10741;
+
+
+select * from aluno;
+
+    
+  
+
+    
